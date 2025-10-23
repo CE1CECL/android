8e49b937cd0112216f026c6cb744d95fca247a15
Christopher Lentocha <christopherericlentocha@gmail.com>
2025-10-23 13:24:52 -0400

    Update HOST_linux-x86.mk

--- build/core/combo/HOST_linux-x86.mk
+++ build/core/combo/HOST_linux-x86.mk
@@ -36,11 +36,16 @@ ifeq ($(TARGET_PRODUCT),sdk)
 HOST_SDK_TOOLCHAIN_PREFIX := prebuilt/linux-x86/toolchain/i686-linux-glibc2.7-4.4.3/bin/i686-linux
 # Don't do anything if the toolchain is not there
 ifneq (,$(strip $(wildcard $(HOST_SDK_TOOLCHAIN_PREFIX)-gcc)))
-HOST_CC  := $(HOST_SDK_TOOLCHAIN_PREFIX)-gcc-4.4
-HOST_CXX := $(HOST_SDK_TOOLCHAIN_PREFIX)-g++-4.4
+HOST_CC  := $(HOST_SDK_TOOLCHAIN_PREFIX)-gcc
+HOST_CXX := $(HOST_SDK_TOOLCHAIN_PREFIX)-g++
 HOST_AR  := $(HOST_SDK_TOOLCHAIN_PREFIX)-ar
 endif # $(HOST_SDK_TOOLCHAIN_PREFIX)-gcc exists
 endif # TARGET_PRODUCT == sdk
+
+ifneq ($(TARGET_PRODUCT),sdk)
+HOST_CC  := gcc-4.4
+HOST_CXX := g++-4.4
+endif
 
 # Does the build machine have the expected gcc version?
 IS_EXPECTED_HOST_GCC := $(filter 4.4.%, $(shell $(HOST_CC) --version))
