8972b1ee12aaab38472caaf1d9e9ef5a0b7b0abf
Christopher Lentocha <christopherericlentocha@gmail.com>
2025-11-30 08:07:40 -0500

    Revert a change
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- frameworks/base/services/surfaceflinger/DisplayHardware/DisplayHardwareBase.cpp
+++ frameworks/base/services/surfaceflinger/DisplayHardware/DisplayHardwareBase.cpp
@@ -78,6 +78,7 @@ bool DisplayHardwareBase::DisplayEventThread::threadLoop()
       err = read(fd, &buf, 1);
     } while (err < 0 && errno == EINTR);
     close(fd);
+    LOGW_IF(err<0, "ANDROID_WAIT_FOR_FB_SLEEP failed (%s)", strerror(errno));
     if (err >= 0) {
         sp<SurfaceFlinger> flinger = mFlinger.promote();
         LOGD("About to give-up screen, flinger = %p", flinger.get());
@@ -92,6 +93,7 @@ bool DisplayHardwareBase::DisplayEventThread::threadLoop()
       err = read(fd, &buf, 1);
     } while (err < 0 && errno == EINTR);
     close(fd);
+    LOGW_IF(err<0, "ANDROID_WAIT_FOR_FB_WAKE failed (%s)", strerror(errno));
     if (err >= 0) {
         sp<SurfaceFlinger> flinger = mFlinger.promote();
         LOGD("Screen about to return, flinger = %p", flinger.get());
