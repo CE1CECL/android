0ad1a99ca70fe2b566daa9f42148ef1a98b9465f
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-03-14 17:24:03 -0400

    Update BoardConfig.mk

--- build/target/board/generic/BoardConfig.mk
+++ build/target/board/generic/BoardConfig.mk
@@ -33,7 +33,7 @@ ARCH_ARM_HAVE_VFP_D32 := false
 BOARD_USES_GENERIC_AUDIO := false
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 
-USE_CAMERA_STUB := false
+USE_CAMERA_STUB := true
 
 # Enable dex-preoptimization to speed up the first boot sequence
 # of an SDK AVD. Note that this operation only works on Linux for now
