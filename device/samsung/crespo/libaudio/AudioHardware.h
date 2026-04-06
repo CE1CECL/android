1bbb8079901d18faa2c96b46807d994e3bce0a4f
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-04-06 08:41:00 -0400

    Update AudioHardware.h

--- device/samsung/crespo/libaudio/AudioHardware.h
+++ device/samsung/crespo/libaudio/AudioHardware.h
@@ -65,7 +65,7 @@ namespace android_audio_legacy {
 // Default audio input sample rate
 #define AUDIO_HW_IN_SAMPLERATE 32000
 // Default audio input channel mask
-#define AUDIO_HW_IN_CHANNELS (AudioSystem::CHANNEL_OUT_STEREO)
+#define AUDIO_HW_IN_CHANNELS (AudioSystem::CHANNEL_IN_STEREO)
 // Default audio input sample format
 #define AUDIO_HW_IN_FORMAT (AudioSystem::PCM_16_BIT)
 // Kernel pcm in buffer size in frames at 32kHz (before resampling)
