ba5945b95ef1d591c4dc8902bdc76d4f241282ba
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-04-11 15:55:42 -0400

    Gfx
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- build/core/combo/arch/x86/x86-atom.mk
+++ build/core/combo/arch/x86/x86-atom.mk
@@ -1,18 +1,4 @@
-# This file contains feature macro definitions specific to the
-# 'x86-atom' arch variant. This is an extension of the 'x86' base variant
-# that adds Atom-specific features.
-#
-# See build/core/combo/arch/x86/x86.mk for differences.
-#
-ARCH_X86_HAVE_MMX   := true
-ARCH_X86_HAVE_SSE   := true
-ARCH_X86_HAVE_SSE2  := true
-ARCH_X86_HAVE_SSE3  := true
-
-ARCH_X86_HAVE_SSSE3 := true
-ARCH_X86_HAVE_MOVBE := true
-ARCH_X86_HAVE_POPCNT := false   # popcnt is not supported by current Atom CPUs
-
-# This flag is used to enabled Atom-specific optimizations with our toolchain
-#
-TARGET_GLOBAL_CFLAGS += -march=atom
+ARCH_X86_HAVE_SSE2 := false
+ARCH_X86_HAVE_SSE3 := false
+ARCH_X86_HAVE_SSSE3 := false
+TARGET_GLOBAL_CFLAGS += -march=i486 -mtune=i386 -mfpmath=387 -mno-mmx -mno-sse
--- system/core/init/property_service.c
+++ system/core/init/property_service.c
@@ -545,19 +545,7 @@ static void load_persistent_properties()
                 close(fd);
                 continue;
             }
-#if 0
-            // File must not be accessible to others, be owned by root/root, and
-            // not be a hard link to any other file.
-            if (((sb.st_mode & (S_IRWXG | S_IRWXO)) != 0)
-                    || (sb.st_uid != 0)
-                    || (sb.st_gid != 0)
-                    || (sb.st_nlink != 1)) {
-                ERROR("skipping insecure property file %s (uid=%lu gid=%lu nlink=%d mode=%o)\n",
-                      entry->d_name, sb.st_uid, sb.st_gid, sb.st_nlink, sb.st_mode);
-                close(fd);
-                continue;
-            }
-#endif
+
             length = read(fd, value, sizeof(value) - 1);
             if (length >= 0) {
                 value[length] = 0;
