a4a7225041a5c0e9109bc07cf30257c035cf53af
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-04-15 15:00:05 -0400

    Updates
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- build/target/product/full_armv5.mk
+++ build/target/product/full_armv5.mk
@@ -20,7 +20,7 @@
 # entirely appropriate to inherit from for on-device configurations.
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/board/generic/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/board/generic_armv5/device.mk)
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
     libreference-ril
 
 # Overrides
-PRODUCT_NAME := full
-PRODUCT_DEVICE := generic
+PRODUCT_NAME := full_armv5
+PRODUCT_DEVICE := generic_armv5
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Full Android on Emulator
