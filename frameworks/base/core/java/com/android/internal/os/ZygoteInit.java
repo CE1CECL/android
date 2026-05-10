96b94e974c0a427afedac149542564a49d6a3ac2
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-05-09 21:48:14 -0400

    Fix inteldrmfb On 1366x768 Res & Disable Preloading
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- frameworks/base/core/java/com/android/internal/os/ZygoteInit.java
+++ frameworks/base/core/java/com/android/internal/os/ZygoteInit.java
@@ -522,7 +522,7 @@ public class ZygoteInit {
             registerZygoteSocket();
             EventLog.writeEvent(LOG_BOOT_PROGRESS_PRELOAD_START,
                 SystemClock.uptimeMillis());
-            preload();
+            //preload();
             EventLog.writeEvent(LOG_BOOT_PROGRESS_PRELOAD_END,
                 SystemClock.uptimeMillis());
 
