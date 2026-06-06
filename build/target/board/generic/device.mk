689cf199b8fdd1901814b48625ce4e0549e6069b
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-06-06 14:45:51 -0400

    Fixes
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- build/target/board/generic/device.mk
+++ build/target/board/generic/device.mk
@@ -23,6 +23,42 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.adb.qemud=1
 
 PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.camera.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.faketouch.multitouch.distinct.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.faketouch.multitouch.distinct.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.faketouch.multitouch.jazzhand.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.faketouch.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.faketouch.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.faketouch.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.location.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.nfc.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/frameworks/native/data/etc/android.hardware.wifi.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/frameworks/native/data/etc/android.software.sip.voip.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/frameworks/native/data/etc/android.software.sip.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/frameworks/native/data/etc/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/frameworks/native/data/etc/com.nxp.mifare.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/frameworks/native/data/etc/handheld_core_hardware.xml \
+    frameworks/native/data/etc/frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/frameworks/native/data/etc/tablet_core_hardware.xml \
+    frameworks/base/data/etc/platform.xml:system/etc/permissions/platform.xml \
     external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
     device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
