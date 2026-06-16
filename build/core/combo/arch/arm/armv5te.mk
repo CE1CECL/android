692f112de5f5dba8cf34b28f556742324ab33b96
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-06-16 00:47:15 -0400

    WIP
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- build/core/combo/arch/arm/armv5te.mk
+++ build/core/combo/arch/arm/armv5te.mk
@@ -1,3 +1,7 @@
+ARCH_ARM_HAVE_ARMV7A := false
+ARCH_ARM_HAVE_NEON := false
 ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_VFP := false
+ARCH_ARM_HAVE_VFP_D32 := false
 FORCE_ARM_DEBUGGING := true
 arch_variant_cflags := -march=armv5te
